e64d5b995a96259322d91616effcf43517ddcfe9
jacqueline kenny <jkennyvasconcelos@hotmail.com>
2024-06-16 18:35:27 -0300

    setup do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
 }
 
 function images() {
